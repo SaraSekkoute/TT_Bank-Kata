c082182ec7a0a22882321b36f3aa1680d450d9d4
Sara SEKKOUTE <148474333+SaraSekkoute@users.noreply.github.com>
2025-01-30 14:59:51 +0100

    Last version

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -4,27 +4,44 @@ import java.time.LocalDate;
 
 public class Transaction {
 
-        private  int amount;
-        private  String type;
-        private LocalDate date;
+    private int amount;
+    private String type;
+    private LocalDate date;
 
+    public Transaction(int amount, String type, LocalDate date) {
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+    }
 
-        public Transaction(int amount, String type, LocalDate date) {
-            this.amount = amount;
-            this.type = type;
-            this.date = date;
-        }
     public Transaction(int amount, String type) {
         this.amount = amount;
         this.type = type;
     }
 
-        public int getAmount() {
-            return amount;
-        }
+    // Getters and Setters
+    public int getAmount() {
+        return amount;
+    }
 
-        public LocalDate getDate() {
-            return date;
-        }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
